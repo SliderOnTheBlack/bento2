082028a57a58692dce623f985869f5f7192fb02f
SliderOnTheBlack <105185690+SliderOnTheBlack@users.noreply.github.com>
2023-11-06 09:07:22 +1030

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: 'bffa12d920bedf073db4be5f48d1473f', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
